6f1392d3241ddab5684fdfe180e020c77da89ccd
Lucas-Soares100 <lucas.soares3@edu.fecap.br>
2025-10-14 11:09:05 -0300

    Base do jogo

---
+++ ProjetoPL/Assets/Scripts/Player/Player Movement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime *Speed;
+    }
+}
