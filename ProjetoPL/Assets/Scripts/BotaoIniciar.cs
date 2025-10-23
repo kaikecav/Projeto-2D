2dd02b93bd3e9fc757f68dd3258a06a8a186273e
Guilherme Belcastro Medeiros <guilhermebelcastr12@gmail.com>
2025-10-23 18:30:45 -0300

    Menu Principal
    Menu Principal do jogo.

---
+++ ProjetoPL/Assets/Scripts/BotaoIniciar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotaoIniciar : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do jogo");
+        Application.Quit();
+    }
+}
